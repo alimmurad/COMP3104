abfd0d87c6777bd98539c46650ef2d0d77f6d36c
Murad Alimov <murad.alimov@georgebrown.ca>
2024-10-02 19:54:54 -0400

    .gitignore added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: StudentID || Student Name || DevOps</h5>
+      <h5>Developed By: 101256125 || Murad Alimov || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
